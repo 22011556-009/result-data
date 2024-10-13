9b63d4f59b3c4fa8bb3e909d376bd06cb58e071b
maryam 009 <ewwme2902@gmail.com>
2024-10-13 15:34:10 +0500

    Add project files.

---
+++ result data/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace result_data
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            button1.Location = new Point(22, 350);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 40);
+            button1.TabIndex = 0;
+            button1.Text = "INSERT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btn_insert_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            button2.Location = new Point(151, 350);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 40);
+            button2.TabIndex = 1;
+            button2.Text = "NEW";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btn_new_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            button3.Location = new Point(290, 350);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 40);
+            button3.TabIndex = 2;
+            button3.Text = "UPDATE";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += btn_update_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            button4.Location = new Point(424, 350);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 40);
+            button4.TabIndex = 3;
+            button4.Text = "DELETE";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += btn_delete_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            button5.Location = new Point(550, 350);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 40);
+            button5.TabIndex = 4;
+            button5.Text = "EXIT";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += btn_exit_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label1.Location = new Point(12, 87);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 19);
+            label1.TabIndex = 5;
+            label1.Text = "Course Code";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label2.Location = new Point(147, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 19);
+            label2.TabIndex = 6;
+            label2.Text = "Course Title";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label3.Location = new Point(290, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 19);
+            label3.TabIndex = 7;
+            label3.Text = "Obt. Marks";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label4.Location = new Point(437, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 19);
+            label4.TabIndex = 8;
+            label4.Text = "Grade";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            label5.Location = new Point(550, 87);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 19);
+            label5.TabIndex = 9;
+            label5.Text = "Status";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(-1, 150);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(639, 183);
+            dataGridView1.TabIndex = 10;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 119);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(91, 25);
+            textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(138, 119);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(97, 25);
+            textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(290, 119);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(83, 25);
+            textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(424, 119);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(75, 25);
+            textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(535, 119);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(77, 25);
+            textBox5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+            label6.Location = new Point(225, 20);
+            label6.Name = "label6";
+            label6.Size = new Size(143, 25);
+            label6.TabIndex = 16;
+            label6.Text = "(22011556-051)";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Cyan;
+            ClientSize = new Size(637, 401);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 10F);
+            Name = "Form1";
+            Text = "Malaika Kashaf";
+            Load += form1_load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label6;
+    }
+}
